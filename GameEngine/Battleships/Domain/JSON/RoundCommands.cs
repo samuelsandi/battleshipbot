3d802d34623c82a2859e6a2e75afb7c1a8732dd8
untungtanujaya <utanujaya@gmail.com>
2018-02-12 19:09:26 +0700

    apalah namanya

--- GameEngine/Battleships/Domain/JSON/RoundCommands.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Domain.JSON
-{
-    public class RoundCommands
-    {
-        public Player1Move Player1Move { get; set; }
-        public Player2Move Player2Move { get; set; }
-    }
-}
